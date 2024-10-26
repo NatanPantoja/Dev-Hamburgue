a4fcecc0c85355587758babcb474d0c281413452
NatanPantoja <natanaelletras@gmail.com>
2024-10-26 18:34:35 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'sans': ['Roboto' , 'sans-seif']
+    },
+    extend: {
+
+      colors:{
+        'prime-brown': '#3D2C24', 
+        'prime-gold': '#D4AF37',
+        'prime-cinza': '#B0B0B0'   
+      },
+
+      backgroundImage:{
+        "home": "url('/src/img/fundo.png')"
+      }
+    },
+  },
+  plugins: [],
+}
